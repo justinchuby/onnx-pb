ab69ae1f995ab83140bfefb0aba475f3d08dd9b0
Justin Chu <justinchu@microsoft.com>
2024-08-07 20:48:46 +0000

    Generate

--- generate.sh
+++ generate.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-protoc --proto_path=onnx --python_out=src/onnx_pb  onnx.proto --pyi_out=src/onnx_pb
+protoc --proto_path=onnx/onnx --python_out=src/onnx_pb  onnx.proto --pyi_out=src/onnx_pb
